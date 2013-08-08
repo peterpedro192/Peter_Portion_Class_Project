3bfa10c0908b932e8766309d73059fca2759fb1a
designstudio_2 <designstudio_2@192.168.1.195>
2013-08-08 13:43:20 +0100

    Added index file

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php include('includes/head.inc');?>
+  <title>Beautiful Wedding Photography by Arrow Photography</title>
+  <!--USING LESS METHOD - Styles go here, Links then embedded -->
+  <script src="js/modernizr.min.js"></script>
+  <link rel="stylesheet/less" href="css/lessversion.less"/>
+  <script src="js/less.min.js"></script>
+</head>
+<body>
+<div>
+    <!-- Header starts -->
+    <div id="header">
+        <h1>Arrow Wedding and Portrait Photography</h1>
+       <!-- REMOVING NAV LIST
+        <ul class="nav">    
+            <li><a href="index.html">Home</a></li>
+            <li><a href="faqs.html">FAQs</a></li>
+            <li><a href="testimonials.html">Testimonials</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>  
+       -->
+       <?php include('includes/nav.inc');?>
+    </div>
+    <!-- Header ends -->
+    
+    <!-- Content starts -->
+    <div id="content">
+        <h2>Beautiful weddings</h2>
+        
+        <p>
+            <img src="images/flowers.gif" alt="Grey scale daisey image" width="300" height="209" />
+            <img src="images/flowers.gif" alt="Grey scale daisey image" />
+        </p>
+        <div id="first" class="section">
+            <h3>How I help make your special day</h3>
+                <p>
+                  I have been a professional photographer for 15 years, 
+                  specialising in <strong>modern and stylish wedding 
+                  photography</strong>. You will want photographs of 
+                  yourselves that are <em>creative, romantic and 
+                  artistically</em> taken &hellip; which is why <q>I 
+                  leave nothing to chance</q> and photograph everything 
+                  that is magical &ndash; <span>just</span> as you wish it to be. 
+                </p>
+        </div>
+        <div id="second" class="section">
+            <h3>Friendly and professional service</h3>
+                <p>
+                  Your wedding photographs will be something that you, your
+                  families and friends will treasure forever. <br />
+                  My style is friendly and relaxed enabling me to capture 
+                  all those special <span>moments</span> throughout the day with the 
+                  minimum of fuss.
+                </p>
+        
+
+                <p>
+                  For more information, 
+                  <a href="/Class_Project/contact.html">
+                      please contact me
+                  </a>.
+                </p>
+        </div>
+    </div>          
+    <!-- Content ends -->
+    <?php include('includes/foot.inc');?>
+
+</div>    
+</body>
+</html>
